4865204b0e0f5ab1fa9bd633f09f8000e029722c
Pham Hoang Duy Long <phamduylong1742000@gmail.com>
2023-04-26 23:44:10 +0700

    learn useState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './views/Nav';
+
+import React, { useState } from 'react';
 
 function App() {
+
+  // state
+  let [name, setName] = useState("Long");
+  let [data, setData] = useState("");
+
+  const change = (event) => {
+    setData(event.target.value);
+  }
+
+  const clicked = (event) => {
+    setName(data);
+  }
+
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Long
+          Hello {name}
         </p>
+        <input type='text' onChange={(event) => { change(event) }}></input>
+        <button onClick={(event) => { clicked(event) }}>Send data</button>
       </header>
     </div>
   );
